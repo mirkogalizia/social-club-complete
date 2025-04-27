b427ce614a194524532285f1cec54f5335ef0520
mirkogalizia <mirko.galizia@outlook.com>
2025-04-27 11:16:32 +0200

    fix(firebase): corrected admin sdk for vercel environment

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,17 +1,20 @@
 // firebase/admin.ts
 
-import * as admin from 'firebase-admin'
+import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  })
+const firebaseAdminConfig = {
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
 }
 
-const db = admin.firestore()
+const firebaseAdminApp = !getApps().length
+  ? initializeApp(firebaseAdminConfig)
+  : getApp()
 
-export { admin, db }
+const db = getFirestore(firebaseAdminApp)
+
+export { firebaseAdminApp, db }
